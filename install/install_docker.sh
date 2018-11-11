eded5aec6a3a33686fd9154685b50d6f6751c39f
Alex Coleman <8428797+LordMailman@users.noreply.github.com>
2018-11-11 15:40:08 -0500

    install_docker.sh now also installs docker-compose.

--- install/install_docker.sh
+++ install/install_docker.sh
@@ -31,3 +31,8 @@ sudo groupadd docker
 
 # Test docker install
 # sudo docker run hello-world
+
+# Install Docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
